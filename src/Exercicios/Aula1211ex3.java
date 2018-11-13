02edd386721f7d53e9f63ff723cdb3af1f861d55
Dalicaria Cristina Silva de OLiveira_Cesar Vinicius Prado Luiz <dalicariaztd1@gmail.com>
2018-11-13 20:34:10 -0200

    Correção da classe de operações aritméticas

--- src/Exercicios/Aula1211ex3.java
+++ src/Exercicios/Aula1211ex3.java
@@ -15,9 +15,11 @@ public class Aula1211ex3 {
     public static void main(String[] args) {
         int nmr1=0, nmr2=0, conta = 0, result = 0;
         while(conta!=5){
+           conta=Integer.parseInt(JOptionPane.showInputDialog("Escolha a operação:\n1-Soma\n2-Subtração\n-3Multiplicação\n4-Divisão\n5-Sair"));
+           if(conta!=5){
            nmr1=Integer.parseInt(JOptionPane.showInputDialog("Digite o 1° número:")); 
            nmr2=Integer.parseInt(JOptionPane.showInputDialog("Digite o 2° número:"));
-           conta=Integer.parseInt(JOptionPane.showInputDialog("Escolha a operação:\n1-Soma\n2-Subtração\n-3Multiplicação\n4-Divisão\n5-Sair"));
+           }
            if(conta==1){
               result=nmr1+nmr2;
            }
@@ -30,7 +32,9 @@ public class Aula1211ex3 {
            if(conta==4){
               result=nmr1/nmr2;
            }
+           if(conta!=5)
+                JOptionPane.showMessageDialog(null,"O resultado é:"+Integer.toString(result));
         }
-        JOptionPane.showMessageDialog(null,"O resultado é:"+Integer.toString(result));
+        
     }
 }
